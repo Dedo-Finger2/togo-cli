690af8b60aafd3355843452367fa939e8928878b
antonio mauricio <antonioimportant@gmail.com>
2024-08-09 11:41:33 -0300

    feat: add listing all tasks

--- cmd/main.go
+++ cmd/main.go
@@ -225,6 +225,9 @@ func listTasks() {
 		case "--completed":
 			listCompletedOnly()
 			return
+		case "--all":
+			listAllTasks()
+			return
 		default:
 			fmt.Println("invalid argument. listing uncompleted tasks instead.")
 		}
@@ -275,6 +278,53 @@ func listTasks() {
 		if taskCompleted == "true" {
 			continue
 		}
+
+		fmt.Println(strings.Join(formatedLine[:], "\t"))
+	}
+}
+
+func listAllTasks() {
+	// Get current user
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	// Create output
+	outputPath := path.Join(user.HomeDir, "Documents", "ToGoLists")
+
+	files, err := os.ReadDir(outputPath)
+	if err != nil {
+		panic(err)
+	}
+
+	userToGoList := files[0].Name()
+
+	file, err := os.Open(filepath.Join(outputPath, userToGoList))
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+
+	content, err := csvReader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("-------------------------------")
+	fmt.Println("# - " + userToGoList)
+	fmt.Println("-------------------------------")
+	for _, line := range content {
+		var (
+			taskID        = line[0]
+			taskName      = line[1]
+			taskCreatedAt = strings.Split(line[2], " ")[0] // Removes time
+			taskCompleted = line[3]
+			formatedLine  = [4]string{taskID, taskName, taskCreatedAt, taskCompleted}
+		)
 
 		fmt.Println(strings.Join(formatedLine[:], "\t"))
 	}
