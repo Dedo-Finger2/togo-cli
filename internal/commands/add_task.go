74ff67efe0a4d321de0d8d717c6035e1c4e50ac2
antonio mauricio <antonioimportant@gmail.com>
2024-08-13 15:06:34 -0300

    refactor: Moves some logic from add_tasks to new utils funcs

--- internal/commands/add_task.go
+++ internal/commands/add_task.go
@@ -1,15 +1,10 @@
 package commands
 
 import (
-	"encoding/json"
-	"fmt"
 	"log/slog"
 	"os"
-	"path"
-	"path/filepath"
 	"time"
 
-	"github.com/Dedo-Finger2/todo-list-cli/internal/errors"
 	"github.com/Dedo-Finger2/todo-list-cli/internal/types"
 	"github.com/Dedo-Finger2/todo-list-cli/internal/utils"
 )
@@ -20,80 +15,43 @@ func AddTask() {
 
 	utils.Validator("Task-Name", &taskName, []string{"not-null"})
 
-	// Get current user
-	user, err := utils.GetCurrentUser()
+	files, err := utils.GetAllFilesInToGoListFile()
 	if err != nil {
-		slog.Error("Error trying to get current user.", "error", err)
+		slog.Error("Failed to get all files from ToGoList folder.", "error", err)
 		os.Exit(1)
 	}
 
-	// Create output
-	outputPath := path.Join(user.HomeDir, "Documents", "ToGoLists")
-
-	// Read all files in the to do list dir
-	files, err := os.ReadDir(outputPath)
+	toGoListFolderPath, err := utils.GetToGoListFolderPath()
 	if err != nil {
-		slog.Error("Error trying to get files in ToGoList DIR", "error", err)
+		slog.Error("Failed to get ToGoList folder path.", "error", err)
 		os.Exit(1)
 	}
 
-	if len(files) < 2 {
-		errors.ResourceNotFound("To-go list")
+	if err = utils.CheckToGoListFolderContent(&files); err != nil {
+		slog.Error("Failed on validating required files in ToGoList folder.", "error", err)
 		os.Exit(1)
 	}
 
-	// Get the first file name.extension
 	userToGoListFile := files[0].Name()
-
-	// Get current time
 	currentDateTime := time.Now().Local().Format(time.DateTime)
 
-	// Read the content of the ids.json
-	jsonData, err := os.ReadFile(filepath.Join(outputPath, "ids.json"))
+	lastInsertedTaskID, err := utils.GetLastIdUsed()
 	if err != nil {
-		slog.Error("Error trying to read ids.json file in ToGoList DIR.", "error", err)
+		slog.Error("Failed on trying to get last used ID.", "error", err)
 		os.Exit(1)
 	}
 
-	// Payload structure
-	type JsonPayload struct {
-		ID int64 `json:"id"`
-	}
-
-	// Payload
-	var payload JsonPayload
-
-	// Parse json into the variable
-	err = json.Unmarshal(jsonData, &payload)
-	if err != nil {
-		slog.Error("Error trying to parse json content from ids.json into a variable.", "error", err)
-		os.Exit(1)
-	}
-
-	// Create a new task
 	task := types.Task{
-		ID:        payload.ID + 1,
+		ID:        lastInsertedTaskID + 1,
 		Name:      taskName,
 		CreatedAt: currentDateTime,
 		Completed: false,
 	}
 
-	// Update the ID
-	utils.UpdateLastIdStored(payload.ID + 1)
+	utils.UpdateLastIdStored(lastInsertedTaskID + 1)
 
-	// Open to do list file
-	file, err := os.OpenFile(filepath.Join(outputPath, userToGoListFile), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		slog.Error("Error trying to open to-go list file.", "error", err)
-		os.Exit(1)
-	}
-
-	defer file.Close()
-
-	// Appends task to the to do list file
-	_, err = file.WriteString(fmt.Sprintf("\n%d,%s,%s,%t", task.ID, task.Name, task.CreatedAt, task.Completed))
-	if err != nil {
-		slog.Error("Error trying to update to-do list file.", "error", err)
+	if err = utils.WriteTaskInToGoListFile(&task, toGoListFolderPath, userToGoListFile); err != nil {
+		slog.Error("Failed on trying to write task in ToGo List file.", "error", err)
 		os.Exit(1)
 	}
 
