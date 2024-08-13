30ee82ce62ab7cd0314e001a2ff89d45123db9ea
antonio mauricio <antonioimportant@gmail.com>
2024-08-13 09:32:55 -0300

    feat: Adds os.Exit 1 in error handling functions

--- internal/errors/main.go
+++ internal/errors/main.go
@@ -2,12 +2,15 @@ package errors
 
 import (
 	"log/slog"
+	"os"
 )
 
 func ResourceCannotBeEmpty(resourceName string) {
 	slog.Warn("Resource cannot be empty.", "resource", resourceName)
+	os.Exit(1)
 }
 
 func ResourceNotFound(resourceName string) {
 	slog.Error("Resource not found.", "resource", resourceName)
+	os.Exit(1)
 }
